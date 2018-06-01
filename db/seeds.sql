a03c7c039624740c8917970fb2bbb9bdfd045d96
Joseph Kim <josephikim2@gmail.com>
2018-06-01 10:32:46 -0700

    basic starter files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pineapple Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
